4a0adb4000990923137fe79729339a98daa240fb
王英 <hzwangying1@corp.netease.com>
2019-01-24 14:58:43 +0800

    change: 下载相关的例子

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const fse = require('fse');
 const static = require('koa-static');
 const opn = require('opn');
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-// 配置静态资源
-app.use(static(
-    path.join(__dirname, './src')
-))
-
-// 请求
-router.get('/xhr/doc/:name', async (ctx, next) => {
+const returnFile = async (ctx, next) => {
+    console.log(ctx.request.body);// post 方法提交的参数
     let filePath = decodeURI(ctx.path.replace('/xhr/', ''));
     if (fse.existsSync(path.resolve(filePath))) {
         ctx.set('Content-disposition', 'attachment; filename=a.pptx');
@@ -25,6 +21,22 @@ router.get('/xhr/doc/:name', async (ctx, next) => {
         console.log('error');
         await next();
     }
+}
+
+// 配置静态资源
+app.use(static(
+    path.join(__dirname, './src')
+))
+
+// 使用ctx.body解析中间件
+app.use(bodyParser())
+
+// 请求
+router.get('/xhr/doc/:name', returnFile)
+router.post('/xhr/doc/:name', returnFile)
+// 请求
+router.get('/xhr/get/:name', async (ctx, next) => {
+    ctx.body = '/xhr/doc/test.pptx'
 })
 
 app
